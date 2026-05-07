f493320c3f9f9b989af96923c099c2b3ad48bbea
Pablo Leone <info@tallerdedigitalizacion.com>
2026-05-07 16:41:15 +0200

    Publish static export for GitHub Pages

---
+++ _next/static/chunks/app/sobre-nosotros/page-2d662d6dcd7d5d8b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[104,386,974],{7731:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,2619,23)),Promise.resolve().then(n.t.bind(n,1356,23))}},e=>{e.O(0,[850,441,255,358],()=>e(e.s=7731)),_N_E=e.O()}]);
